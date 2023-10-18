8741013ba62168691ffceb92ae1f8d0661949fa1
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-10-18 23:25:50 +0900

    Feat 글로벌 스타일 생성

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,127 @@
+import { createGlobalStyle } from 'styled-components';
+
+import gaeguBold from '../assets/fonts/Gaegu-Bold.ttf';
+import gaeguLight from '../assets/fonts/Gaegu-Light.ttf';
+import gaeguRegular from '../assets/fonts/Gaegu-Regular.ttf';
+
+import himelodyRegular from '../assets/fonts/HiMelody-Regular.ttf';
+import nanumRegular from '../assets/fonts/NanumPenScript-Regular.ttf';
+
+const GlobalStyle = createGlobalStyle`
+  #root{
+    min-height: 100vh;
+    width: 100%;
+    font-size: 2rem;
+    font-family: 'Noto Sans KR', sans-serif;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+    box-sizing: border-box;
+    min-height: 100%;
+    width: 100%;
+    
+
+    background-color: var(--bg-color);
+    color: var(--font-color);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow-x: scroll;
+  }
+
+  html {
+    height: 100%;
+    
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+  color: inherit;
+  text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  * {
+    box-sizing: inherit;
+
+    ::-webkit-scrollbar {
+      display: none;
+    }
+  }
+  
+  button {
+    border: none;
+    outline: none;
+    cursor: pointer;
+
+    &:focus {
+      outline: 0;
+    }
+  }
+
+  .sr-only {
+  overflow: hidden;
+  position: absolute;
+  clip: rect(0, 0, 0, 0);
+  clip-path: polygon(0 0, 0 0, 0 0);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+}
+
+@font-face {
+  font-family: 'Gaegu';
+  font-weight: 700;
+  src: local('Gaegu'), url(${gaeguBold}) format('truetype');
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Gaegu';
+  font-weight: 400;
+  src: local('Gaegu'), url(${gaeguLight}) format('truetype');
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Gaegu';
+  font-weight: 500;
+  src: local('Gaegu'), url(${gaeguRegular}) format('truetype');
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Hi melody';
+  font-weight: 500;
+  src: local('Hi melody'), url(${himelodyRegular}) format('truetype');
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Nanum Pen Script';
+  font-weight: 500;
+  src: local('Nanum Pen Script'), url(${nanumRegular}) format('truetype');
+  font-display: swap;
+}
+`;
+
+export default GlobalStyle;
