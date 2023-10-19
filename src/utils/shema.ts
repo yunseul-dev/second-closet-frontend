b6b477d36d32288f78380fba0f1b1db98a7cc2ef
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-10-20 03:24:13 +0900

    Refactor: SignIn 스키마 수정

--- src/utils/shema.ts
+++ src/utils/shema.ts
@@ -4,13 +4,13 @@ const useridRegExp = /^[A-Za-z0-9]{4,20}/;
 const passwordRegExp = /^[A-Za-z0-9]{6,12}$/;
 
 const signInSchema = z.object({
-  userid: z.string().regex(useridRegExp, '이메일 형식에 맞게 입력해 주세요.'),
-  password: z.string().regex(passwordRegExp, '영문 또는 숫자를 6~12자 입력하세요.'),
+  userid: z.string().regex(/^\S/, 'id를 입력하세요.'),
+  password: z.string().regex(/^\S/, 'password를 입력하세요.'),
 });
 
 const signUpSchema = z
   .object({
-    userid: z.string().regex(useridRegExp, '이메일 형식에 맞게 입력해 주세요.'),
+    userid: z.string().regex(useridRegExp, '영문 또는 숫자를 4~20자 입력하세요.'),
     password: z.string().regex(passwordRegExp, '영문 또는 숫자를 6~12자 입력하세요.'),
     confirmPassword: z.string(),
   })
