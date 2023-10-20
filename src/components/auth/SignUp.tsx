4d7fa8a4c1925af33d6b8361e9102b8ac404ff2f
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-10-20 17:45:28 +0900

    Feat: 회원가입 기능 구현

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,38 +1,131 @@
 import { styled } from 'styled-components';
+import { useForm, useController, Control } from 'react-hook-form';
+import { signUpSchema } from '../../utils/shema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { debounce } from 'lodash';
+import { useCallback, ChangeEvent } from 'react';
+// import { BiCheckCircle, BiXCircle } from 'react-icons/bi';
+
+interface SignUpFormData {
+  userid: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+interface InputProps {
+  placeholder: string;
+  control: Control<SignUpFormData>;
+  name: 'userid' | 'password' | 'passwordConfirm';
+  trigger: any;
+}
+
+const InputContainer = ({ placeholder, control, name, trigger }: InputProps) => {
+  const {
+    field: { onChange },
+    fieldState: { invalid, isDirty, error },
+  } = useController({
+    name,
+    control,
+    defaultValue: '',
+  });
+
+  const debouncedTrigger = useCallback(
+    debounce(() => {
+      trigger(name);
+      if (name === 'password') trigger('passwordConfirm');
+    }, 100),
+    [],
+  );
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e);
+    debouncedTrigger();
+  };
+
+  return (
+    <InputWrapper>
+      <Input
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        type={name.toLowerCase().includes('password') ? 'password' : 'text'}
+        autoComplete="off"
+        onChange={handleChange}
+      />
+      {error && <ErrorMsg>{error?.message}</ErrorMsg>}
+    </InputWrapper>
+  );
+};
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const {
+    control,
+    handleSubmit,
+    trigger,
+    formState: { isValid, errors },
+  } = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      userid: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      const { data: user } = await axios.post('api/auth/signup', data, { withCredentials: true });
+      console.log(user);
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Container>
-      <FlexWrapper>
-        <img src="./assets/icons/closetFav.png" alt="Closet Icon" />
-        <Title>세컨클로젯</Title>
-      </FlexWrapper>
-      <CombinedSignBtns>
-        <SignInBtn>Sign In</SignInBtn>
-        <SignUpBtn>Sign Up</SignUpBtn>
-      </CombinedSignBtns>
-      <form>
-        <FormWrapper>
-          <ContentContainer placeholder="id" />
-          <ContentContainer placeholder="password" />
-          <ContentContainer placeholder="password confirm" />
-          <SubmitBtn type="submit">Sign up</SubmitBtn>
-        </FormWrapper>
-      </form>
-    </Container>
+    <>
+      <Container>
+        <FlexWrapper>
+          <img src="./assets/icons/closetFav.png" alt="Closet Icon" />
+          <Title>세컨클로젯</Title>
+        </FlexWrapper>
+        <CombinedSignBtns>
+          <SignInBtn>Sign In</SignInBtn>
+          <SignUpBtn>Sign Up</SignUpBtn>
+        </CombinedSignBtns>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormWrapper>
+            <InputContainer placeholder={'아이디'} control={control} name={'userid'} trigger={trigger} />
+            <InputContainer placeholder={'비밀번호'} control={control} name={'password'} trigger={trigger} />
+            <InputContainer
+              placeholder={'비밀번호 확인'}
+              control={control}
+              name={'passwordConfirm'}
+              trigger={trigger}
+            />
+            <SubmitBtn type="submit">Sign up</SubmitBtn>
+          </FormWrapper>
+        </form>
+      </Container>
+    </>
   );
 };
 
 export default SignUp;
 
 const Container = styled.div`
-  width: 30%;
-  height: 60vh;
+  min-width: 400px;
+  min-height: 500px;
+  width: 25%;
+  height: 56vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border: 5px solid #fad4db;
+  border: 3px solid #fad4db;
   border-radius: 10px;
 `;
 
@@ -54,11 +147,11 @@ const FormWrapper = styled.div`
   justify-content: center;
   align-items: center;
   background-color: #fad4db;
-  padding: 10px;
+  padding: 20px;
   border-top-left-radius: 20px;
   border-bottom-left-radius: 20px;
   border-bottom-right-radius: 20px;
-  width: 300px;
+  width: 360px;
 `;
 
 const CombinedSignBtns = styled.div`
@@ -66,15 +159,17 @@ const CombinedSignBtns = styled.div`
 `;
 
 const SignInBtn = styled.button`
-  width: 150px;
+  width: 180px;
   height: 40px;
   font-weight: 800;
   padding: 10px;
   background-color: white;
+  font-size: 16px;
 `;
 
 const SignUpBtn = styled.button`
-  width: 150px;
+  font-size: 16px;
+  width: 180px;
   height: 40px;
   font-weight: 800;
   padding: 10px;
@@ -98,25 +193,41 @@ const SignUpBtn = styled.button`
   }
 `;
 
-const ContentContainer = styled.input`
-  width: 280px;
+const Input = styled.input`
+  width: 320px;
   height: 40px;
-  margin-bottom: 10px;
   border-radius: 20px;
   border: none;
-  padding: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
   border: 1px solid gray;
+  margin-bottom: 0;
+  font-size: 16px;
 `;
 
 const SubmitBtn = styled.button`
-  width: 280px;
+  width: 320px;
   height: 40px;
   margin-bottom: 10px;
   border-radius: 20px;
   font-weight: 700;
+  font-size: 16px;
   border: none;
   padding: 10px;
   border: 1px solid gray;
   background-color: #f1899c;
   color: white;
+`;
+
+const ErrorMsg = styled.span`
+  font-size: 13px;
+  padding: 2px 10px 0 10px;
+  color: #d40e0e;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  margin: 10px;
 `;
