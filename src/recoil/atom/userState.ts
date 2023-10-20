ee6f81f29d9b594bb1292dbe3b0b76b8f209753d
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-10-21 01:06:12 +0900

    Feat: userState atom에 localStorage 동기화 추가

--- src/recoil/atom/userState.ts
+++ src/recoil/atom/userState.ts
@@ -2,5 +2,12 @@ import { atom } from 'recoil';
 
 export const userState = atom({
   key: 'userState',
-  default: null,
+  default: localStorage.getItem(localStorage.getItem('userInfo') || '{}'),
+  effects_UNSTABLE: [
+    ({ onSet }) => {
+      onSet(newUser => {
+        localStorage.setItem('userInfo', JSON.stringify(newUser));
+      });
+    },
+  ],
 });
