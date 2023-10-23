8850d454b68e2683fc7e7602393909f4ca9fca4a
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-10-23 20:31:41 +0900

    Feat: App Layout 구현

---
+++ src/components/common/AppLayout.tsx
@@ -0,0 +1,33 @@
+import Header from './Header';
+import React from 'react';
+import styled from 'styled-components';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const AppLayout = ({ children }: LayoutProps) => {
+  return (
+    <LayoutContainer>
+      <Header />
+      <Container>{children}</Container>
+    </LayoutContainer>
+  );
+};
+
+export default AppLayout;
+
+const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;ㅁ
+  width: 60%;
+  height: 100vh;
+`;
+
+const Container = styled.div`
+  flex-grow: 1;
+  width: 100%;
+  overflow: auto;
+  padding: 10px;
+  margin-top: 30px;
+`;
