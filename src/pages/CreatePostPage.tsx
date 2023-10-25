1791b64398a74fcab51db485e8bc038fdabb6411
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-10-26 02:21:04 +0900

    Feat: 게시글 등록 페이지 구현

---
+++ src/pages/CreatePostPage.tsx
@@ -0,0 +1,12 @@
+import AppLayout from '../components/common/AppLayout';
+import CreatePost from '../components/posts/CreatePost';
+
+const Root = () => {
+  return (
+    <AppLayout>
+      <CreatePost />
+    </AppLayout>
+  );
+};
+
+export default Root;
