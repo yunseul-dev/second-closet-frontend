692011122a056290452cb4c5df9e2328c5825841
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-10-26 23:23:21 +0900

    Feat: 상세 페이지 서버 연결

--- src/components/posts/DetailPost.tsx
+++ src/components/posts/DetailPost.tsx
@@ -1,14 +1,91 @@
 import styled from 'styled-components';
-import { LiaHomeSolid, LiaAngleRightSolid } from 'react-icons/lia';
+import { LiaHomeSolid, LiaAngleRightSolid, LiaHeartSolid, LiaHeart } from 'react-icons/lia';
+import { AiFillAlert, AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { LuClock3 } from 'react-icons/lu';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+type Product = {
+  productId: number;
+  userId: string;
+  productName: string;
+  imgs: string[];
+  categories: string[];
+  count: number;
+  price: string;
+  discount: boolean;
+  delivery: boolean;
+  description: string;
+  tags: string[];
+  exchange: boolean;
+  size: string;
+  facetoface: boolean;
+  createdAt: number;
+  hearts: number;
+};
 
 const DetailPost = () => {
+  const [product, setProduct] = useState(null);
+  const [clickHeart, setClickHeart] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/products/1');
+        setProduct(response.data[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!product) {
+    return <div>Loading...</div>; // 데이터를 기다리는 동안 로딩 상태를 표시할 수 있습니다.
+  }
+
+  const handleHeartClick = () => setClickHeart(!clickHeart);
+
+  const {
+    productId,
+    userId,
+    productName,
+    imgs,
+    categories,
+    count,
+    price,
+    discount,
+    delivery,
+    description,
+    tags,
+    exchange,
+    size,
+    facetoface,
+    createdAt,
+    hearts,
+  }: Product = product;
+
+  console.log('imgs: ', imgs);
+
+  let time = '';
+
+  const lastTime = (Date.now() - createdAt) / 1000 / 60 / 60; // 시
+
+  if (Math.floor(lastTime) > 24) {
+    time = `${Math.floor(lastTime / 24)}일 전`; // 날짜
+  } else if (Math.floor(lastTime) > 0) {
+    time = `${lastTime}시간 전`; //
+  } else if (lastTime * 60 > 0) {
+    time = `${Math.floor(lastTime * 60)}분 전`;
+  }
   return (
     <ContainerWrapper>
       <CategoryContainer>
         <LiaHomeSolid />홈
         <LiaAngleRightSolid />
         <Select>
-          <option value="">선택</option>
+          <option value="">{categories[0]}</option>
           <option value="여성의류">여성의류</option>
           <option value="여성의류">남성의류</option>
           <option value="여성의류">가방</option>
@@ -17,7 +94,7 @@ const DetailPost = () => {
         </Select>
         <LiaAngleRightSolid />
         <Select>
-          <option value="">선택</option>
+          <option value="">{categories[1]}</option>
           <option value="아우터">아우터</option>
           <option value="상의">상의</option>
           <option value="바지">바지</option>
@@ -27,7 +104,7 @@ const DetailPost = () => {
         </Select>
         <LiaAngleRightSolid />
         <Select>
-          <option value="">선택</option>
+          <option value="">{categories[2]}</option>
           <option value="아우터">아우터</option>
           <option value="상의">상의</option>
           <option value="바지">바지</option>
@@ -39,58 +116,76 @@ const DetailPost = () => {
       <Container>
         <SubmitConatiner>
           <ImageContainer>
-            <Image src={'./assets/image/party.png'} alt="Image Preview" />
+            {imgs.map(img => (
+              <Image key={img} src={`http://localhost:5023/api/products/uploads/${img}`} alt="Image Preview" />
+            ))}
           </ImageContainer>
           <InfoWrapper>
             <InfoContainer>
-              <ProductName>글로니 트위드 스커트 GLOWNY U BABY TWEED SKIRT</ProductName>
+              <ProductName>{productName}</ProductName>
               <ProductPrice>
-                92,000<Span>원</Span>
+                {price}
+                <Span>원</Span>
               </ProductPrice>
+              <Alarms>
+                <Alarm>
+                  <LiaHeartSolid /> {hearts}
+                </Alarm>
+                <Alarm>
+                  <LuClock3 /> {time}
+                </Alarm>
+                <Alarm>
+                  <AiFillAlert /> 신고하기
+                </Alarm>
+              </Alarms>
               <Infos>
                 <List>
                   <InfoName>배송옵션 </InfoName>
-                  <Info>무료배송</Info>
+                  <Info>{delivery ? '무료배송' : '배송비 별도'}</Info>
                 </List>
                 <List>
                   <InfoName>상품상태 </InfoName>
-                  <Info>새상품</Info>
+                  <Info>{count}</Info>
                 </List>
                 <List>
                   <InfoName>사이즈 </InfoName>
-                  <Info>S</Info>
+                  <Info>{size.toUpperCase()}</Info>
                 </List>
                 <List>
                   <InfoName>교환여부 </InfoName>
-                  <Info>가능</Info>
+                  <Info>{exchange ? '교환가능' : '교환불가'}</Info>
                 </List>
                 <List>
-                  <InfoName>카테고리 </InfoName>
-                  <Info>스커트</Info>
+                  <InfoName>가격제안 </InfoName>
+                  <Info>{discount ? '가격제안 가능' : '가격제안 불가'}</Info>
+                </List>
+                <List>
+                  <InfoName>직거래 </InfoName>
+                  <Info>{facetoface ? '직거래가능' : '직거래불가'}</Info>
                 </List>
               </Infos>
             </InfoContainer>
             <Buttons>
               <TalkBtn>문의하기</TalkBtn>
               <BuyBtn>구매하기</BuyBtn>
-              <HeartBtn>♡ 찜</HeartBtn>
+              <HeartBtn onClick={handleHeartClick}>{clickHeart ? <AiFillHeart /> : <AiOutlineHeart />} 찜</HeartBtn>
             </Buttons>
           </InfoWrapper>
         </SubmitConatiner>
         <ExplainContainer>
-          <h3>판매자 코멘트</h3>
+          <MiniTitle>판매자 코멘트</MiniTitle>
           <Explain>
-            깨끗이 착용했습니다. 저보다 더 잘 입으실 수 있으신 분이 입으시면 좋겠어요 !
+            {description}
             <Tags>
-              {' '}
-              <Tag>#글로니</Tag>
-              <Tag>#글로니스커트</Tag>
+              {tags.map(tag => {
+                return <Tag>#{tag}</Tag>;
+              })}
             </Tags>
           </Explain>
         </ExplainContainer>
       </Container>
       <Bottom>
-        <h3>추천 상품</h3>
+        <MiniTitle>연관 상품</MiniTitle>
         <Recs>
           <Rec>
             <RecImg>사진</RecImg>
@@ -122,10 +217,11 @@ export default DetailPost;
 const ProductName = styled.div`
   font-size: 24px;
   font-weight: 600;
+  padding-bottom: 10px;
 `;
 
 const ProductPrice = styled.div`
-  font-size: 40px;
+  font-size: 38px;
   font-weight: 500;
 `;
 
@@ -135,53 +231,9 @@ const Span = styled.span`
   margin-left: 5px;
 `;
 
-const Tags = styled.div`
-  margin: 20px 0 20px 0;
-`;
+const ContainerWrapper = styled.div``;
 
-const Tag = styled.span`
-  padding: 10px;
-  color: #f77a8f;
-  background-color: white;
-  margin-right: 5px;
-  border-radius: 20px;
-  font-weight: 600;
-  font-size: 16px;
-`;
-
-const Bottom = styled.div`
-  border-top: 1px solid black;
-  padding: 20px;
-  font-size: 20px;
-`;
-
-const Recs = styled.div`
-  display: flex;
-`;
-const Rec = styled.div`
-  width: 20%;
-  height: 20vh;
-  padding: 10px;
-`;
-
-const RecImg = styled.div`
-  width: 100%;
-  height: 100%;
-  border: 1px solid gray;
-`;
-
-const RecName = styled.div`
-  padding: 2px;
-  font-size: 18px;
-`;
-
-const ContainerWrapper = styled.div`
-  padding: 10px;
-`;
-
-const Container = styled.div`
-  padding: 20px;
-`;
+const Container = styled.div``;
 
 const CategoryContainer = styled.div`
   border-bottom: 1px solid #767676;
@@ -203,10 +255,13 @@ const SubmitConatiner = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: 20px;
+  gap: 20px;
 `;
 
 const ImageContainer = styled.div`
   width: 35%;
+  min-width: 420px;
+  min-height: 428px;
   height: 45vh;
   border: solid 1px #dbd9d9;
 `;
@@ -217,30 +272,45 @@ const Image = styled.img`
   object-fit: cover;
 `;
 
+const Alarms = styled.div`
+  border-top: 1px solid #d0d0d0;
+  font-size: 16px;
+  margin: 20px 0 0 0;
+  padding-top: 20px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Alarm = styled.div`
+  color: #a8a7a7;
+  margin: 0 10px 0 10px;
+`;
+
 const InfoWrapper = styled.div`
   width: 60%;
+  min-width: 420px;
+  min-height: 428px;
 `;
 
 const InfoContainer = styled.div`
   width: 100%;
-  height: 35vh;
+  min-height: 350px;
   padding: 0 30px 0 30px;
 `;
 
 const Infos = styled.div`
-  margin: 20px 0 10px 0;
-  border-top: 1px solid #d0d0d0;
   padding-top: 20px;
 `;
 
 const List = styled.div`
   display: flex;
-  font-size: 16px;
+  font-size: 14px;
   margin: 10px 0 10px 0;
 `;
 
 const InfoName = styled.div`
   width: 15%;
+  min-width: 120px;
   color: #898989;
 `;
 
@@ -248,50 +318,96 @@ const Info = styled.div``;
 
 const Buttons = styled.div`
   display: flex;
-  justify-content: flex-end;
   gap: 10px;
   margin-top: 10px;
+  position: relative;
+  bottom: 0;
 `;
 
 const TalkBtn = styled.button`
-  width: 25%;
+  width: 33%;
   height: 8vh;
-  border: solid 1px #ecd167;
+  border: solid 1px #f3e29e;
   font-weight: 700;
   font-size: 16px;
-  background-color: #f1dc89;
+  background-color: #f8e9ae;
 `;
 
 const BuyBtn = styled.button`
-  width: 25%;
+  width: 33%;
   height: 8vh;
-  border: solid 1px #f4768d;
+  border: solid 1px #f9c8d0;
   font-weight: 700;
   font-size: 16px;
-  background-color: #f1899c;
-`;
-
-const HeartBtn = styled.button`
-  width: 15%;
-  height: 8vh;
-  border: solid 1px #69b0ef;
-  font-weight: 700;
-  font-size: 16px;
-  background-color: #86c0f2;
-`;
-
-const ExplainContainer = styled.div`
-  margin: 30px 0 20px 0;
-  font-size: 20px;
-  padding: 20px;
-  /* border-top: 1px solid #d0d0d0; */
-  border-radius: 20px;
-  border: 5px solid #fad4db;
   background-color: #fad4db;
 `;
 
+const HeartBtn = styled.button`
+  width: 33%;
+  height: 8vh;
+  border: solid 1px #89c3f7;
+  font-weight: 700;
+  font-size: 16px;
+  background-color: #9ccaf2;
+`;
+
+const ExplainContainer = styled.div`
+  margin: 30px 0 0 0;
+  padding: 20px;
+  border-top: 1px solid #d0d0d0;
+`;
+
 const Explain = styled.div`
-  margin: 10px;
-  /* border-top: 1px solid #d0d0d0; */
-  padding-top: 10px;
+  font-size: 18px;
+`;
+
+const Tags = styled.div`
+  margin: 20px 0 20px 0;
+`;
+
+const Tag = styled.span`
+  padding: 8px;
+  color: white;
+  background-color: #fbc3c3;
+  margin-right: 5px;
+  border-radius: 10px;
+  font-weight: 600;
+  font-size: 14px;
+`;
+
+const Bottom = styled.div`
+  margin-top: 20px;
+  padding: 10px;
+  font-size: 20px;
+  border-top: 1px solid black;
+`;
+
+const Recs = styled.div`
+  display: flex;
+`;
+const Rec = styled.div`
+  width: 20%;
+  height: 20vh;
+  padding: 10px;
+`;
+
+const RecImg = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 1px solid gray;
+`;
+
+const RecName = styled.div`
+  padding: 2px;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+const MiniTitle = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  margin: 20px 0 20px 0;
 `;
