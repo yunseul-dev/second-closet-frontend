71016a5eae57ba5b34c11e4a76b10ef684b745de
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-01 22:27:45 +0900

    Feat: 로그인 유효 상태 가드 구현

---
+++ src/guard/AuthenticationGuard.tsx
@@ -0,0 +1,44 @@
+import { Navigate } from 'react-router-dom';
+// import useAuthenticationQuery from '../hooks/queries/useAuthenticQuery';
+import { ReactNode, useEffect } from 'react';
+import axios from 'axios';
+import { useSetRecoilState, useRecoilState } from 'recoil';
+import { userState } from '../recoil/atom/userState';
+import { isLoginState } from '../recoil/atom/isLoginState';
+
+interface AuthenticationGuardProps {
+  redirectTo: string;
+  element: ReactNode;
+}
+
+const AuthenticationGuard: React.FC<AuthenticationGuardProps> = ({ redirectTo, element }) => {
+  const setUserId = useSetRecoilState(userState);
+  const [isLogin, setIsLogin] = useRecoilState(isLoginState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('/api/auth/verify', { withCredentials: true });
+        const data = res.data;
+
+        if (data && !data.userId) {
+          setUserId(null);
+          setIsLogin(data.isLogin);
+        }
+
+        if (data && data.userId) {
+          setUserId(data.userId);
+          setIsLogin(data.isLogin);
+        }
+      } catch (error) {
+        console.error('Error occurred while verifying authentication:', error);
+      }
+    };
+
+    fetchData();
+  }, [setUserId, setIsLogin]);
+
+  return isLogin ? <>{element}</> : <Navigate to={redirectTo} />;
+};
+
+export default AuthenticationGuard;
