83d27fdbd665b8afdd72b027b8d4029f4def7b2d
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-01 22:28:18 +0900

    Feat: usePopularInfinityQuery 구현

---
+++ src/hooks/queries/usePopularInfiniteQuery.ts
@@ -0,0 +1,32 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface Product {
+  productId: number;
+  productName: string;
+  imgs: string[];
+}
+
+interface FetchResponse {
+  pages: Product[][];
+  pageParams: number[];
+}
+
+const usePopularInfiniteQuery = () => {
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery<FetchResponse, Error, Product[]>({
+    queryKey: ['@populars'],
+    queryFn: async ({ pageParam = 0 }) => {
+      const res = await axios.get<FetchResponse>(`/api/products/populars/${pageParam}`);
+      return res.data;
+    },
+    getNextPageParam: (lastPage: FetchResponse['pages'][0], allPages: FetchResponse['pageParams']) => {
+      const nextPage = allPages.length === 1 ? 1 : allPages.length + 1;
+
+      return lastPage.length !== 0 && nextPage;
+    },
+  });
+
+  return { data, hasNextPage, fetchNextPage };
+};
+
+export default usePopularInfiniteQuery;
