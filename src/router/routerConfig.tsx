4d1eaf76c70b8eb25a3bcbec230537948f76421c
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-01 22:27:02 +0900

    Feat: 라우터 구현

--- src/router/routerConfig.tsx
+++ src/router/routerConfig.tsx
@@ -1,5 +1,45 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Root from '../pages/Root';
+import AuthForm from '../components/auth/AuthForm';
+import DetailPage from '../pages/DetailPage';
+import CreatePostPage from '../pages/CreatePostPage';
+import AuthenticationGuard from '../guard/AuthenticationGuard';
+import { Suspense } from 'react';
 
-const routerConfig = createBrowserRouter([]);
+const routerConfig = createBrowserRouter([
+  {
+    path: '/signin',
+    element: (
+      <Suspense fallback={<div>...loading</div>}>
+        <AuthForm />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/',
+    element: (
+      <Suspense fallback={<div>...loading</div>}>
+        <Root />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/detail/:id',
+    element: (
+      <Suspense fallback={<div>...loading</div>}>
+        <DetailPage />
+      </Suspense>
+    ),
+  },
+
+  {
+    path: '/createpost',
+    element: (
+      <Suspense fallback={<div>...loading</div>}>
+        <AuthenticationGuard redirectTo="/signin" element={<CreatePostPage />} />
+      </Suspense>
+    ),
+  },
+]);
 
 export default routerConfig;
