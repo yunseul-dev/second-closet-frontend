14bb930d7634dffac1a42b8ea0853db21ad2618e
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-03 00:43:44 +0900

    Feat: 카테고리별 무한 스크롤 구현

---
+++ src/hooks/queries/useCategoryInfiniteQuery.ts
@@ -0,0 +1,40 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface Product {
+  productId: number;
+  productName: string;
+  imgs: string[];
+  createdAt: string;
+}
+
+interface FetchResponse {
+  pages: Product[][];
+  pageParams: number[];
+}
+
+const useCategoryInfiniteQuery = (category: string[]) => {
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery<FetchResponse, Product[], unknown>({
+    queryKey: ['@CategoryItem'],
+    queryFn: async ({ pageParam = 0 }) => {
+      console.log(category);
+      const res = await axios.get(`/api/products/category`, {
+        params: {
+          category: category,
+          page: pageParam,
+        },
+      });
+
+      return res.data;
+    },
+    getNextPageParam: (lastPage: Product[], allPages: Product[][]): number | undefined => {
+      const nextPage = allPages.length === 1 ? 1 : allPages.length;
+
+      return lastPage.length !== 0 ? nextPage : undefined;
+    },
+  });
+
+  return { data, hasNextPage, fetchNextPage };
+};
+
+export default useCategoryInfiniteQuery;
