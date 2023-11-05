a412b54b509fb7b21523b77665d2c3eb2cb36150
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-06 04:15:02 +0900

    Feat: useDeleteHeartMutation 구현

---
+++ src/hooks/mutations/useDeleteHeartMutation.ts
@@ -0,0 +1,21 @@
+import useGenericMutation from './useGenericMutation';
+import axios from 'axios';
+
+const deleteHeart = async ({ productId, userId }: { productId: number; userId: string }) => {
+  axios.delete(`/api/products/hearts/${productId}/${userId}`);
+};
+
+const useDeleteHeartMutation = (sortOption: string, productId: number) => {
+  return useGenericMutation({
+    queryKey: ['@MyHearts', sortOption],
+    mutationFn: deleteHeart,
+    onMutate() {
+      return products => ({
+        ...products,
+        pages: products.pages.map(page => page.filter(product => product.productId !== productId)),
+      });
+    },
+  });
+};
+
+export default useDeleteHeartMutation;
