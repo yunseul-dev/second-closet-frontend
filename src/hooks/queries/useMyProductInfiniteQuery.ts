43706dbbcdd0aa3a5b669cf443bfb18aade135be
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-05 17:19:24 +0900

    Feat: 나의 상품 인피니트 쿼리 구현

---
+++ src/hooks/queries/useMyProductInfiniteQuery.ts
@@ -0,0 +1,40 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface Product {
+  productId: number;
+  productName: string;
+  imgs: string[];
+  createdAt: string;
+}
+
+interface FetchResponse {
+  pages: Product[][];
+  pageParams: number[];
+}
+
+const useMyProductInfiiteQuery = (userId: string, sortOption: string) => {
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery<FetchResponse, Product[], unknown>({
+    queryKey: ['@CategoryItem', userId, sortOption],
+    queryFn: async ({ pageParam = 0 }) => {
+      const res = await axios.get(`/api/products/myproducts?sort=${sortOption}`, {
+        params: {
+          userId: userId,
+          page: pageParam,
+        },
+      });
+
+      return res.data;
+    },
+    getNextPageParam: (lastPage: Product[], allPages: Product[][]): number | undefined => {
+      const nextPage = allPages.length === 1 ? 1 : allPages.length;
+
+      return lastPage.length !== 0 ? nextPage : undefined;
+    },
+    select: response => response.pages.flat(),
+  });
+
+  return { data, hasNextPage, fetchNextPage };
+};
+
+export default useMyProductInfiiteQuery;
