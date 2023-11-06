b975d42a69e361be82e1788a9dcc760f0bcb2441
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-06 22:56:47 +0900

    Feat: useMyHeartsInfiniteQuery 구현

---
+++ src/hooks/queries/useMyHeartsInfiniteQuery.ts
@@ -0,0 +1,41 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface Product {
+  productId: number;
+  productName: string;
+  imgs: string[];
+  price: string;
+  createdAt: string;
+}
+
+interface FetchResponse {
+  pages: Product[][];
+  pageParams: number[];
+}
+
+const useMyHeartsInfiniteQuery = (userId: string, sortOption: string) => {
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery<FetchResponse, Product[], unknown>({
+    queryKey: ['@MyHearts', sortOption],
+    queryFn: async ({ pageParam = 0 }) => {
+      const { data } = await axios.get(`/api/products/myhearts?sort=${sortOption}`, {
+        params: {
+          userId: userId,
+          page: pageParam,
+        },
+      });
+
+      return data;
+    },
+    getNextPageParam: (lastPage: Product[], allPages: Product[][]): number | undefined => {
+      const nextPage = allPages.length === 1 ? 1 : allPages.length;
+
+      return lastPage.length !== 0 ? nextPage : undefined;
+    },
+    select: response => response.pages.flat(),
+  });
+
+  return { products: data, hasNextPage, fetchNextPage };
+};
+
+export default useMyHeartsInfiniteQuery;
