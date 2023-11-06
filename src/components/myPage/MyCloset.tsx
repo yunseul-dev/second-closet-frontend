55681331e9fe2fbfc5af6ab0a827d30f239e2fc7
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-06 22:55:07 +0900

    Feat: 카테고리 클릭 기능 구현

--- src/components/myPage/MyCloset.tsx
+++ src/components/myPage/MyCloset.tsx
@@ -3,8 +3,9 @@ import { PiPencilSimpleLineBold } from 'react-icons/pi';
 import { RxDividerVertical } from 'react-icons/rx';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../recoil/atom/userState';
-
+import { useState } from 'react';
 import MyProducts from './MyProducts';
+import MyHearts from './MyHearts';
 
 interface DivProp {
   $bold: boolean;
@@ -20,6 +21,9 @@ interface DivPencilProp {
 
 const MyCloset = () => {
   const userId = useRecoilValue(userState);
+  const [category, setCategory] = useState('products');
+
+  const handleCategoryClick = (category: string) => setCategory(category);
 
   return (
     <Container>
@@ -46,12 +50,24 @@ const MyCloset = () => {
       </MyInfo>
       <MyList>
         <ListTab>
-          <ListName $clicked={true}>상품</ListName>
-          <ListName $clicked={false}>찜</ListName>
-          <ListName $clicked={false}>옷장 후기</ListName>
+          <ListName $clicked={category === 'products'} onClick={() => handleCategoryClick('products')}>
+            상품
+          </ListName>
+          <ListName $clicked={category === 'hearts'} onClick={() => handleCategoryClick('hearts')}>
+            찜
+          </ListName>
+          <ListName $clicked={category === 'reviews'} onClick={() => handleCategoryClick('reviews')}>
+            옷장 후기
+          </ListName>
           <Empty />
         </ListTab>
-        <MyProducts userId={userId} />
+        {category === 'products' ? (
+          <MyProducts userId={userId} />
+        ) : category === 'hearts' ? (
+          <MyHearts userId={userId} />
+        ) : (
+          <></>
+        )}
       </MyList>
     </Container>
   );
@@ -132,90 +148,7 @@ const Empty = styled.div`
   border-bottom: 1px solid #000;
 `;
 
-const ListCount = styled.div`
-  font-size: 18px;
-  padding: 20px 0 20px 0;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const Sort = styled.div`
-  display: flex;
-  margin-right: 20px;
-`;
-
 const Divider = styled.div`
   font-size: 20px;
   color: #a1a0a0;
-`;
-
-const SortTab = styled.div<DivProp>`
-  font-weight: ${({ $bold }) => $bold && '600'};
-  font-size: 14px;
-`;
-
-const ItemContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 30px;
-`;
-
-const Item = styled.div`
-  padding: 30px;
-  width: 48%;
-  height: 350px;
-  border: 1px solid #e0e0e0c4;
-  display: flex;
-`;
-
-const ImageContainer = styled.div`
-  width: 45%;
-  height: 100%;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-
-const ItemInfoContainer = styled.div`
-  font-size: 14px;
-  height: 60px;
-  width: 55%;
-  padding: 10px 20px 10px 20px;
-  position: relative;
-`;
-
-const ItemInfo = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const ItemName = styled.div`
-  width: 100%;
-  font-size: 16px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  margin-bottom: 15px;
-`;
-
-const MiniInfo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  margin: 10px 0;
-`;
-
-const Price = styled.span`
-  font-size: 18px;
-  font-weight: 600;
-`;
-
-const Edit = styled.div`
-  position: absolute;
-  top: 250px;
-  right: 0;
 `;
