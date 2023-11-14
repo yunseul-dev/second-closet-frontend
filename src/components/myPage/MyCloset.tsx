df988c5aea9c7ad0557067fc34e6ac6cd6fc093e
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-14 10:03:44 +0900

    Refactor: AboutProducts 컴포넌트 분리

--- src/components/myPage/MyCloset.tsx
+++ src/components/myPage/MyCloset.tsx
@@ -1,42 +1,12 @@
 import styled from 'styled-components';
 import MyInfo from './MyInfo';
-import { useEffect, useState } from 'react';
-import MyProducts from './MyProducts';
-import MyHearts from './MyHearts';
-import { useQueryClient } from '@tanstack/react-query';
-
-interface DivNameProp {
-  $clicked: boolean;
-}
+import AboutProducts from './AboutProducts';
 
 const MyCloset = () => {
-  const [category, setCategory] = useState('products');
-  const queryClient = useQueryClient();
-
-  const handleCategoryClick = (category: string) => setCategory(category);
-
-  useEffect(() => {
-    queryClient.refetchQueries();
-  }, []);
-
   return (
     <Container>
       <MyInfo />
-      <MyList>
-        <ListTab>
-          <ListName $clicked={category === 'products'} onClick={() => handleCategoryClick('products')}>
-            상품
-          </ListName>
-          <ListName $clicked={category === 'hearts'} onClick={() => handleCategoryClick('hearts')}>
-            찜
-          </ListName>
-          <ListName $clicked={category === 'reviews'} onClick={() => handleCategoryClick('reviews')}>
-            옷장 후기
-          </ListName>
-          <Empty />
-        </ListTab>
-        {category === 'products' ? <MyProducts /> : category === 'hearts' ? <MyHearts /> : <></>}
-      </MyList>
+      <AboutProducts />
     </Container>
   );
 };
@@ -44,29 +14,3 @@ const MyCloset = () => {
 export default MyCloset;
 
 const Container = styled.div``;
-
-const MyList = styled.div``;
-
-const ListTab = styled.div`
-  display: flex;
-`;
-
-const ListName = styled.div<DivNameProp>`
-  width: 200px;
-  height: 48px;
-  font-size: 14px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #e6e6e6;
-
-  font-weight: ${({ $clicked }) => $clicked && '600'};
-  border: ${({ $clicked }) => $clicked && '1px solid #000'};
-  border-bottom: ${({ $clicked }) => ($clicked ? 'none' : '1px solid #000')};
-`;
-
-const Empty = styled.div`
-  width: calc(100% - (200px * 3));
-  height: 48px;
-  border-bottom: 1px solid #000;
-`;
