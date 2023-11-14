18e9d11ca7e2f09cb6e87b3c9d9d58dd7613678f
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-14 11:46:41 +0900

    Refactor: 초기값 빈 객체에서 null로 수정

--- src/recoil/atom/userState.ts
+++ src/recoil/atom/userState.ts
@@ -2,7 +2,7 @@ import { atom } from 'recoil';
 
 export const userState = atom({
   key: 'userState',
-  default: localStorage.getItem('userInfo') || '{}',
+  default: localStorage.getItem('userInfo') || null,
   effects_UNSTABLE: [
     ({ onSet }) => {
       onSet(newUser => {
