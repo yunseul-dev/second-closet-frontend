7292a24dd6c994823dd5f5d9ab79e1af884c814a
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-14 11:46:18 +0900

    Feat: useUserQuery 구현

---
+++ src/hooks/queries/useUserQuery.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../recoil/atom/userState';
+import axios from 'axios';
+
+const staleTime = 1000 * 3;
+
+const fetchData = async (userId: string) => {
+  const { data } = await axios.get(`/api/users/${userId}`);
+  return data;
+};
+
+const useUserQuery = () => {
+  const userId = useRecoilValue(userState);
+
+  const query = useQuery({
+    queryKey: ['@UserInfo', userId],
+    queryFn: () => fetchData(userId || ''),
+    staleTime,
+  });
+
+  return { ...query, userInfo: query.data };
+};
+
+export default useUserQuery;
