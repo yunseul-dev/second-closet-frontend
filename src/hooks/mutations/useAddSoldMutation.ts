f2881f2079d551bcf78847e1401f74b39e3b3149
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-16 17:37:02 +0900

    Feat: useAddSoldMutation 구현

---
+++ src/hooks/mutations/useAddSoldMutation.ts
@@ -0,0 +1,32 @@
+import useGenericMutation from './useGenericMutation';
+import axios from 'axios';
+
+const addSold = async (productId: number) => {
+  axios.patch(`/api/products/update/${productId}`, { sold: true });
+};
+
+const useAddSoldMutation = (sortOption: string, productId: number) => {
+  return useGenericMutation({
+    queryKey: ['@MyProducts', sortOption],
+    mutationFn: addSold,
+    onMutate() {
+      return products => ({
+        ...products,
+        pages: products.pages.map(page =>
+          page.map(product => {
+            if (product.productId === productId) {
+              return {
+                ...product,
+                sold: true,
+              };
+            }
+
+            return product;
+          }),
+        ),
+      });
+    },
+  });
+};
+
+export default useAddSoldMutation;
