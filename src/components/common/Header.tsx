7b6a117bb8c56319ac01702f7ba402442f3fcbaa
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-17 23:05:36 +0900

    Refactor: 구매 메세지 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,10 +4,11 @@ import { useRecoilValue } from 'recoil';
 import { isLoginState } from '../../recoil/atom/isLoginState';
 import { Category } from '../../constants/Category';
 import { useState } from 'react';
-import { LuShirt, LuUser2 } from 'react-icons/lu';
+import { LuUser2 } from 'react-icons/lu';
 import { BiCloset } from 'react-icons/bi';
 import { RxDividerVertical } from 'react-icons/rx';
 import { PiSignInBold } from 'react-icons/pi';
+import { AiOutlineSearch, AiOutlineMessage } from 'react-icons/ai';
 import CategoryContainer from './CategoryContainer';
 import useAuthenticationQuery from '../../hooks/queries/useAuthenticQuery';
 
@@ -20,6 +21,7 @@ const Header = () => {
 
   const handleLogoClick = () => navigate('/');
   const handleSigninClick = () => navigate('/signin');
+  const handleMessageClick = () => navigate('/buypage');
   const handleCreatepostClick = () => navigate('/createpost');
   const handleMypageClick = () => navigate('/mypage');
 
@@ -27,51 +29,85 @@ const Header = () => {
   const handleMouseLeave = () => setIsHovered(false);
 
   return (
-    <>
-      <Container>
-        <FirstContainer>
-          <FaBarsWrapper onMouseEnter={handleMouseEnter}>
-            <LuShirt />
-          </FaBarsWrapper>
-          <Title onClick={handleLogoClick}>SecondCloset</Title>
-          <SearchBar>
-            <Input type="text" placeholder="  상품명을 입력하세요"></Input>
-          </SearchBar>
-          {isLogin ? (
-            <BtnContainer>
-              <Btn onClick={handleCreatepostClick}>
-                <BiCloset />
-                <div>상품 등록</div>
-              </Btn>
-              <Divider>
-                <RxDividerVertical />
-              </Divider>
-              <Btn onClick={handleMypageClick}>
-                <LuUser2 />
-                <div>My 옷장</div>
-              </Btn>
-            </BtnContainer>
-          ) : (
-            <Btn onClick={handleSigninClick}>
-              <PiSignInBold />
-              <div> Sign In</div>
+    <Container>
+      <FirstContainer>
+        <Logo>
+          <LogoImg src="/assets/image/Logo.png" alt="logo" onClick={handleLogoClick}></LogoImg>
+        </Logo>
+        <SearchBar>
+          <Input type="text" placeholder="상품명을 입력하세요." />
+          <SearchIcon />
+        </SearchBar>
+        {isLogin ? (
+          <BtnContainer>
+            <Btn onClick={handleMessageClick}>
+              <AiOutlineMessage />
+              <div>메세지</div>
             </Btn>
-          )}
-        </FirstContainer>
-        <div onMouseLeave={handleMouseLeave}>
-          <CategoryList onMouseEnter={handleMouseEnter}>
-            {Object.keys(Category).map(category => (
-              <CategoryName key={category}>{category}</CategoryName>
-            ))}
-          </CategoryList>
-          {isHovered && <CategoryContainer />}
-        </div>
-      </Container>
-    </>
+            <Divider>
+              <RxDividerVertical />
+            </Divider>
+            <Btn onClick={handleCreatepostClick}>
+              <BiCloset />
+              <div>상품 등록</div>
+            </Btn>
+            <Divider>
+              <RxDividerVertical />
+            </Divider>
+            <Btn onClick={handleMypageClick}>
+              <LuUser2 />
+              <div>My 옷장</div>
+            </Btn>
+          </BtnContainer>
+        ) : (
+          <Btn onClick={handleSigninClick}>
+            <PiSignInBold />
+            <div> Sign In</div>
+          </Btn>
+        )}
+      </FirstContainer>
+      <div onMouseLeave={handleMouseLeave}>
+        <CategoryList onMouseEnter={handleMouseEnter}>
+          {Object.keys(Category).map(category => (
+            <CategoryName key={category}>{category}</CategoryName>
+          ))}
+        </CategoryList>
+        {isHovered && <CategoryContainer />}
+      </div>
+    </Container>
   );
 };
 
 export default Header;
+
+const Container = styled.header`
+  padding-top: 3%;
+  position: fixed;
+  top: 0;
+  width: 1240px;
+  height: 160px;
+  z-index: 999;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #d4d4d4;
+  background-color: #fff;
+`;
+
+const FirstContainer = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+  width: 100%;
+`;
+
+const Logo = styled.div`
+  margin-left: 20px;
+`;
+
+const LogoImg = styled.img`
+  width: 200px;
+  height: auto;
+  display: flex;
+  align-items: center;
+`;
 
 const BtnContainer = styled.div`
   display: flex;
@@ -84,23 +120,6 @@ const Divider = styled.div`
   color: #a1a0a0;
 `;
 
-const Container = styled.header`
-  margin-top: 4%;
-  position: sticky;
-  top: 0;
-  width: 100%;
-  z-index: 999;
-  padding-bottom: 10px;
-  border-bottom: 1px solid #d4d4d4;
-  position: relative;
-`;
-
-const FirstContainer = styled.div`
-  display: flex;
-  margin-bottom: 20px;
-  width: 100%;
-`;
-
 const CategoryList = styled.div`
   width: 75%;
   height: 30px;
@@ -110,28 +129,13 @@ const CategoryList = styled.div`
 
 const CategoryName = styled.div`
   font-size: 14px;
-  font-weight: 600;
-  color: #747373;
+  font-weight: 700;
+  /* color: #747373; */
+  /* color: #ff4d24; */
   display: flex;
   width: 20%;
   align-items: center;
   justify-content: center;
-`;
-
-const Title = styled.div`
-  font-family: 'Gaegu';
-  font-weight: 600;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 36px;
-`;
-
-const FaBarsWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: 8px;
-  font-size: 38px;
 `;
 
 const SearchBar = styled.form`
@@ -139,23 +143,38 @@ const SearchBar = styled.form`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  input[type='text']::placeholder {
+    color: #ff4d24;
+  }
+  position: relative;
 `;
 
 const Input = styled.input`
   width: 85%;
   height: 85%;
-  border: 2px solid #fc7b7b;
-  border-radius: 20px;
+  border: 2px solid #ff4d24;
+  border-radius: 10px;
+  padding-left: 15px;
+`;
+
+const SearchIcon = styled(AiOutlineSearch)`
+  position: absolute;
+  right: 9%;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 20px;
+  color: #ff4d24;
 `;
 
 const Btn = styled.div`
   border-radius: 5%;
   margin: 0 5px 0 5px;
   padding: 10px 10px 10px 10px;
-  font-weight: 600;
+  font-weight: 700;
   display: flex;
   font-size: 16px;
   justify-content: center;
   align-items: center;
-  gap: 5px;
+  gap: 4px;
 `;
