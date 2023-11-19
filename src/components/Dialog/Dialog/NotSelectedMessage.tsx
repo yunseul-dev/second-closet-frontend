82daa58f7b7bbbb8d417e46ba2d4fea9d0f1009a
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-19 21:41:02 +0900

    Feat: NotSelectedMessage 컴포넌트 구현

---
+++ src/components/Dialog/Dialog/NotSelectedMessage.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { LuMessagesSquare } from 'react-icons/lu';
+
+const NotSelectedMessage = () => {
+  return (
+    <None>
+      <MessageIcon />
+      <div>대화방을 클릭해주세요!</div>
+    </None>
+  );
+};
+
+export default NotSelectedMessage;
+
+const None = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+`;
+
+const MessageIcon = styled(LuMessagesSquare)`
+  color: #ff4d24;
+  margin: 5px;
+`;
