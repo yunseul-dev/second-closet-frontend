b62963ec2e675ac85c4e548f665a562f05e89f09
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-20 20:09:36 +0900

    Feat: 소켓 호출

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:5023');
+export const SocketContext = createContext(socket);
+
+socket.on('connect', () => {
+  console.log('connected to server');
+});
+
+socket.on('message', data => {
+  console.log(data);
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
