15b91c033f1538b67bd3d1044537c5baba309ac0
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-20 16:00:45 +0900

    Feat: socket을 이용한 채팅 update

---
+++ src/hooks/mutations/useSendMessage.ts
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { SocketContext } from '../../main';
+
+const useSendMessage = () => {
+  const socket = useContext(SocketContext);
+
+  const sendMessage = ({
+    messageId,
+    userId,
+    textValue,
+  }: {
+    messageId: string;
+    userId: string | null;
+    textValue: string;
+  }) => {
+    socket.emit('message', { messageId, userId, textValue });
+  };
+
+  return sendMessage;
+};
+
+export default useSendMessage;
