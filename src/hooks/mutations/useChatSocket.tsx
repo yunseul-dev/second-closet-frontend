6457af66f4d8146cc1dbfc2f214430b7743e9b64
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-20 16:00:28 +0900

    Feat: 소켓으로 업데이트 된 데이터 받기 구현

---
+++ src/hooks/mutations/useChatSocket.tsx
@@ -0,0 +1,33 @@
+import { Dispatch, SetStateAction, useContext, useEffect } from 'react';
+import { SocketContext } from '../../main';
+
+type Messages = {
+  senderId: string;
+  message: string;
+  timestamp: number;
+};
+
+type Message = {
+  userId: string;
+  textValue: string;
+};
+
+const useChatSocket = (messageId: string, setChatMessages: Dispatch<SetStateAction<Messages[]>>) => {
+  const socket = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.on('message', (newMessage: Message) => {
+      const { userId, textValue } = newMessage;
+      setChatMessages(prevMessages => [
+        ...prevMessages,
+        { senderId: userId, message: textValue, timestamp: Date.now() },
+      ]);
+    });
+
+    return () => {
+      socket.off('message');
+    };
+  }, [socket, messageId, setChatMessages]);
+};
+
+export default useChatSocket;
