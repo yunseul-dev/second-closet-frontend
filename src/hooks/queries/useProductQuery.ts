a30ceba8f6c8e1b7083515a630a2075d1ac3ec9f
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-24 22:23:04 +0900

    Fix: 잘못된 쿼리키 수정

--- src/hooks/queries/useProductQuery.ts
+++ src/hooks/queries/useProductQuery.ts
@@ -1,26 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
-import { useRecoilValue } from 'recoil';
-import { userState } from '../../recoil/atom/userState';
 import axios from 'axios';
 
 const fetchProduct = async (id: string) => {
-  const { data } = await axios.get(`/api/products/${id}`);
-  return data;
+  const res = await axios.get(`/api/products/${id}`);
+  return res.data;
 };
 
 const staleTime = 1000;
 
-const useProductQuery = (id: string, options: object) => {
-  const userId = useRecoilValue(userState);
-
+const useProductQuery = (id: string) => {
   const query = useQuery({
-    queryKey: ['@ProductInfo', userId],
+    queryKey: ['@ProductInfo', id],
     queryFn: () => fetchProduct(id),
     staleTime,
-    ...options,
   });
 
-  return { ...query, productInfo: query.data[0] };
+  return { productInfo: query.data[0] };
 };
 
 export default useProductQuery;
