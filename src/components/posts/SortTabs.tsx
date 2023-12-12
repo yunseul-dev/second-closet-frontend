64ff3b95a1ec8a0939d34f502463eb9858af5598
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-12-12 14:58:14 +0900

    Rename: 파일 이동

---
+++ src/components/posts/SortTabs.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+import { RxDividerVertical } from '../../utils/icons';
+interface SortTabsProps {
+  setSortOption: (sortOption: string) => void;
+  sortOption: string;
+  name: string;
+}
+
+interface DivProp {
+  $bold: boolean;
+}
+
+const SortTabs: React.FC<SortTabsProps> = ({ setSortOption, sortOption, name }) => {
+  const handleSortClick = (sortOption: string) => setSortOption(sortOption);
+
+  return (
+    <ItemsName>
+      <CategoryName>
+        <SpanTitle>"{name}"</SpanTitle>의 전체상품
+      </CategoryName>
+      <Sort>
+        <SortTab onClick={() => handleSortClick('latest')} $bold={sortOption === 'latest'}>
+          최신순
+        </SortTab>
+        <Divider>
+          <RxDividerVertical />
+        </Divider>
+        <SortTab onClick={() => handleSortClick('popular')} $bold={sortOption === 'popular'}>
+          인기순
+        </SortTab>
+        <Divider>
+          <RxDividerVertical />
+        </Divider>
+        <SortTab onClick={() => handleSortClick('highPrice')} $bold={sortOption === 'highPrice'}>
+          고가순
+        </SortTab>
+        <Divider>
+          <RxDividerVertical />
+        </Divider>
+        <SortTab onClick={() => handleSortClick('lowPrice')} $bold={sortOption === 'lowPrice'}>
+          저가순
+        </SortTab>
+      </Sort>
+    </ItemsName>
+  );
+};
+
+export default SortTabs;
+
+const SpanTitle = styled.span`
+  font-weight: 600;
+`;
+
+const CategoryName = styled.div``;
+
+const ItemsName = styled.div`
+  margin: 50px 20px 25px 0;
+  font-size: 16px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Divider = styled.div`
+  font-size: 20px;
+  color: #a1a0a0;
+`;
+
+const Sort = styled.div`
+  font-size: 16px;
+  display: flex;
+`;
+
+const SortTab = styled.div<DivProp>`
+  font-weight: ${({ $bold }) => $bold && '600'};
+`;
