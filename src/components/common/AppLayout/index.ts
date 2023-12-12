ee3b90d871a1332ed3981f81c2f555ec9c50f113
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-12-12 13:17:56 +0900

    Feat: Settings 컴포넌트 및 Search 컴포넌트 export 추가

--- src/components/common/AppLayout/index.ts
+++ src/components/common/AppLayout/index.ts
@@ -3,3 +3,5 @@ export { default as AppLayout } from './AppLayout';
 export { default as ArrowUpBtn } from './ArrowUpBtn';
 export { default as CategoryContainer } from './CategoryContainer';
 export { default as Header } from './Header';
+export { default as Settings } from './Settings';
+export { default as Search } from './Search';
