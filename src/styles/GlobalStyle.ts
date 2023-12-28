3aa3c4c1691895afcf65709c8656cd760e980786
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-12-29 05:36:51 +0900

    Refactor: 사용하지 않는 폰트 삭제

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,17 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
-import blackWoff from '../assets/fonts/Pretendard-Black.woff';
-import blackWoff2 from '../assets/fonts/Pretendard-Black.woff2';
-
 import boldWoff from '../assets/fonts/Pretendard-Bold.woff';
 import boldWoff2 from '../assets/fonts/Pretendard-Bold.woff2';
 import extraBoldWoff from '../assets/fonts/Pretendard-ExtraBold.woff';
 import extraBoldWoff2 from '../assets/fonts/Pretendard-ExtraBold.woff2';
-
-import lightWoff from '../assets/fonts/Pretendard-Light.woff';
-import lightWoff2 from '../assets/fonts/Pretendard-Light.woff2';
-import extraLightWoff from '../assets/fonts/Pretendard-ExtraLight.woff';
-import extraLightWoff2 from '../assets/fonts/Pretendard-ExtraLight.woff2';
 
 import mediumWoff from '../assets/fonts/Pretendard-Medium.woff';
 import mediumWoff2 from '../assets/fonts/Pretendard-Medium.woff2';
@@ -22,30 +14,8 @@ import regularWoff2 from '../assets/fonts/Pretendard-Regular.woff2';
 import semiBoldWoff from '../assets/fonts/Pretendard-SemiBold.woff';
 import semiBoldWoff2 from '../assets/fonts/Pretendard-SemiBold.woff2';
 
-import thinWoff from '../assets/fonts/Pretendard-Thin.woff';
-import thinWoff2 from '../assets/fonts/Pretendard-Thin.woff2';
-
 const GlobalStyle = createGlobalStyle`
-@font-face {
-  font-family: 'Pretendard';
-  font-weight: 100;
-  src: local('Pretendard Thin'), url(${thinWoff}) format('woff'),url(${thinWoff2}) format('woff2');
-  font-display: swap;
-}
 
-@font-face {
-  font-family: 'Pretendard';
-  font-weight: 200;
-  src: local('Pretendard Extra Bold'),url(${extraLightWoff}) format('woff'),url(${extraLightWoff2}) format('woff2');
-  font-display: swap;
-}
-
-@font-face {
-  font-family: 'Pretendard';
-  font-weight: 300;
-  src: local('Pretendard Light'), url(${lightWoff}) format('woff'),url(${lightWoff2}) format('woff2');
-  font-display: swap;
-}
 
 @font-face {
   font-family: 'Pretendard';
@@ -82,12 +52,7 @@ const GlobalStyle = createGlobalStyle`
   font-display: swap;
 }
 
-@font-face {
-  font-family: 'Pretendard';
-  font-weight: 900;
-  src: local('Pretendard Black'), url(${blackWoff}) format('woff'),url(${blackWoff2}) format('woff2');
-  font-display: swap;
-}
+
 
   #root{
     min-height: 100vh;
