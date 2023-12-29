3fc6cba27f5b6063bd8a8cbbbb4fa8aadf9a6022
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-12-29 14:55:29 +0900

    Feat: 성능 최적화를 위한 롤업옵션 및 이미지 최적화

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,47 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminWebp from 'imagemin-webp';
+import imageminSvgo from 'imagemin-svgo';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngQuant from 'imagemin-pngquant';
+import imageminGifSicle from 'imagemin-gifsicle';
 
 const PORT = 5023;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+        gif: imageminGifSicle(),
+        svg: imageminSvgo(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+          jpeg: imageminWebp(),
+        },
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id: string) => {
+          if (id.includes('node_modules')) {
+            const module = id.split('node_modules/').pop().split('/')[0];
+            return `vendor/${module}`;
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1024 * 500,
+  },
   server: {
     proxy: {
       '/api': {
