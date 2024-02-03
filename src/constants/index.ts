4468d176475611fbb7992293a7998fb38627dca0
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-02-03 18:44:19 +0900

    Feat: payments 상수 구현

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export { default as Category } from './Category';
 export { sizes, shoesSizes } from './sizes';
 export { default as wearCounts } from './wearCounts';
 export { banks } from './banks';
+export { payments } from './payments';
